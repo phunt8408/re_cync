f49c7a5f668ed0e5bd5741823120c19f77179950
phunt8408 <iampatrickhunt@gmail.com>
2024-11-12 16:21:41 -0500

    Add files via upload

--- custom_components/re_cync/coordinator.py
+++ custom_components/re_cync/coordinator.py
@@ -22,7 +22,6 @@ API_DEVICE_PROPS = (
     "https://api.gelighting.com/v2/product/{product_id}/device/{device_id}/property"
 )
 
-
 DEVICE_TYPES_SWITCHES = {55, 68}
 DEVICE_TYPES_BULBS = {57, 146}
 
@@ -43,7 +42,7 @@ class ReCyncCoordinator(DataUpdateCoordinator):
         hass: HomeAssistant,
         entry: ConfigEntry,
     ) -> None:
-        """Create the SIAHub."""
+        """Initialize the ReCync Coordinator."""
         _LOGGER.debug("Hub init")
         super().__init__(
             hass,
@@ -65,24 +64,22 @@ class ReCyncCoordinator(DataUpdateCoordinator):
 
         url = API_DEVICE_LIST.format(user_id=self._rcs.user_id)
         devices = await self._get_url(url)
+        for d in devices:
+            # Log the name, ID, and product ID of each device
+            _LOGGER.debug("Discovered device - Name: %s, ID: %s, Product ID: %s", 
+                          d.get("name"), d.get("id"), d.get("product_id"))
 
-    for d in devices:
-        # Log the name, ID, and product ID of each device
-        _LOGGER.debug("Discovered device - Name: %s, ID: %s, Product ID: %s", 
-                      d.get("name"), d.get("id"), d.get("product_id"))
-
-        sku_type = int(d["product_id"], 16) % 1000
-        match sku_type:
-            case 713, 721:
-                _LOGGER.debug("Ignoring switch/dimmer (?) SKU type %d", sku_type)
-            case 897:
-                await self._discover_home(d)
-            case _:
-                _LOGGER.debug("Ignoring SKU type %d (%s)", sku_type, d.get("name"))
-    self._event_stream.set_update_callback(self.async_handle_status)
-    await self._event_stream.initialize()
-    _LOGGER.info("Cloud started")
-
+            sku_type = int(d["product_id"], 16) % 1000
+            match sku_type:
+                case 713, 721:
+                    _LOGGER.debug("Ignoring switch/dimmer (?) SKU type %d", sku_type)
+                case 897:
+                    await self._discover_home(d)
+                case _:
+                    _LOGGER.debug("Ignoring SKU type %d (%s)", sku_type, d.get("name"))
+        self._event_stream.set_update_callback(self.async_handle_status)
+        await self._event_stream.initialize()
+        _LOGGER.info("Cloud started")
 
     async def async_handle_status(self, switch_id, status) -> None:
         _LOGGER.debug("Got status %s %s", switch_id, status)
@@ -108,8 +105,6 @@ class ReCyncCoordinator(DataUpdateCoordinator):
             + ((430 + int(mesh_id[0]) + int(mesh_id[1])) % 256).to_bytes(1, "big")
             + bytes.fromhex("7e")
         )
-        # packet = bytes([0x73, 0x00, 0x00, 0x00, 0x1F])
-        # packet += int(switch_id).to_bytes(4, "big") + int(self._seq).to_bytes(2, "big")
 
         await self._event_stream.async_command(
             bytes.fromhex("730000001f"), switch_id, packet
@@ -132,12 +127,24 @@ class ReCyncCoordinator(DataUpdateCoordinator):
 
     async def _discover_home(self, device):
         url = API_DEVICE_PROPS.format(
-        product_id=device["product_id"], device_id=device["id"]
-    )
-    info = await self._get_url(url)
-    for bulb in info["bulbsArray"]:
-        # Log details of each bulb discovered
-        _LOGGER.debug("Bulb found - Name: %s, ID: %s, Type: %s", 
-                      bulb.get("displayName"), bulb.get("deviceID"), bulb.get("deviceType"))
-        self._bulbs.append(bulb)
+            product_id=device["product_id"], device_id=device["id"]
+        )
+        info = await self._get_url(url)
+        for bulb in info["bulbsArray"]:
+            # Log details of each bulb discovered
+            _LOGGER.debug("Bulb found - Name: %s, ID: %s, Type: %s", 
+                          bulb.get("displayName"), bulb.get("deviceID"), bulb.get("deviceType"))
+            self._bulbs.append(bulb)
 
+    async def _get_url(self, url):
+        headers = {"Access-Token": self._rcs.access_token}
+        async with aiohttp.ClientSession() as s, s.get(url, headers=headers) as resp:
+            data = await resp.json()
+            _LOGGER.debug("GetUrl %s -> %s", url, data)
+            match resp.status:
+                case 200:
+                    return data
+                case 403:
+                    raise AuthError("Forbidden", url, resp.status, data)
+                case _:
+                    raise ApiError("Failed to fetch", url, resp.status, data)
